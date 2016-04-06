c459da68333858709a4ba48dab042f4259491309
amontan <amontan@us.ibm.com>
2016-04-06 12:47:33 -0500

    Extra comma fix

--- open-docs.go
+++ open-docs.go
@@ -49,7 +49,7 @@ func (OpenDocsPlugin) GetMetadata() plugin.PluginMetadata {
 				HelpText: "open app swagger url in browser",
 				UsageDetails: plugin.Usage{
 					Usage: "docs <appname>",
-				},
+				}
 			}
 		},
 	}
